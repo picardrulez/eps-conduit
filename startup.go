cdd941a30a5200192947a170b546f004e5b87bb9
Geoff Scott <picardrulez@gmail.com>
2020-03-13 13:41:53 -0400

    adding debug info for dbcleaner research

--- startup.go
+++ startup.go
@@ -4,11 +4,17 @@ import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"strconv"
 	"time"
 )
 
 func startup() int {
-	database, _ := sql.Open("sqlite3", "./epsconduit.db")
+	database, err := sql.Open("sqlite3", "./epsconduit.db")
+	if err != nil {
+		log.Println("error opening database for startup procedures")
+		log.Printf("%s", err)
+		return 1
+	}
 	routesTableCreate, err := database.Prepare("CREATE TABLE IF NOT EXISTS routes (destination TEXT, insertdate DATETIME)")
 	if err != nil {
 		log.Println("error preparing create routes table statement")
@@ -32,7 +38,11 @@ func startup() int {
 
 func dbCleaner(table string) {
 	log.Println("starting db cleaner on " + table)
-	db, _ := sql.Open("sqlite3", "./epsconduit.db")
+	db, err := sql.Open("sqlite3", "./epsconduit.db")
+	if err != nil {
+		log.Println("error opening database for dbCleaner on + " + table)
+		log.Printf("%s", err)
+	}
 	for {
 		stmt, err := db.Prepare("DELETE from " + table + " where insertdate <= date('now', '-1 hour')")
 		if err != nil {
@@ -40,12 +50,14 @@ func dbCleaner(table string) {
 			log.Println(err)
 			return
 		}
-		_, err = stmt.Exec()
+		stmtrtn, err := stmt.Exec()
 		if err != nil {
 			log.Println("error executing delete from " + table)
 			log.Println(err)
 			return
 		}
+		rowsAffected, _ := stmtrtn.RowsAffected()
+		log.Println("DBCLEANER FOR TABLE " + table + " STATEMENT RETURN ROWS AFFECTED: " + strconv.FormatInt(rowsAffected, 10))
 		time.Sleep(1 * time.Minute)
 	}
 }
